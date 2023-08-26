988f67cc4464391d69dc083a120f177cc99aa218
Oleksii <laziutaoleksij@gmail.com>
2023-08-26 16:36:26 +0300

    adding softmax activation function

--- sample neuron.py
+++ sample neuron.py
@@ -1,36 +1,50 @@
 import numpy as np
 import nnfs
 from nnfs.datasets import spiral_data
-
 nnfs.init()
 
-X, y = spiral_data(samples=100, classes=3)
+class Layer_Dense:
+    def __init__(self, n_inputs, n_neurons):
+    # Initialize weights and biases
+        self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+    # Forward pass
+    def forward(self, inputs):
+        # Calculate output values from inputs, weights and biases
+        self.output = np.dot(inputs, self.weights) + self.biases
 
-# ReLU activation
 class Activation_ReLU:
     # Forward pass
     def forward(self, inputs):
-        # Calculate output values from input
+        # Calculate output values from inputs
         self.output = np.maximum(0, inputs)
 
-class Layer_Dense():
-    def __init__(self, n_inputs, n_neurons) -> None:
-        self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
-        self.biases = np.zeros((1, n_neurons))
-
+class Activation_Softmax:
+    # Forward pass
     def forward(self, inputs):
-        self.output = np.dot(inputs, self.weights) + self.biases
-
+    # Get unnormalized probabilities
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        # Normalize them for each sample
+        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+        self.output = probabilities
+        
+# Create dataset
+X, y = spiral_data(samples=100, classes=3)
 
 dense1 = Layer_Dense(2, 3)
 
-# Create ReLU activation (to be used with Dense layer):
 activation1 = Activation_ReLU()
-# Perform a forward pass of our training data through this layer
-dense1.forward(X)
-# Forward pass through activation func.
-# Takes in output from previous layer
-activation1.forward(dense1.output)
-# Let's see output of the first few samples:
-print(activation1.output[:5])
 
+dense2 = Layer_Dense(3, 3)
+
+activation2 = Activation_Softmax()
+
+dense1.forward(X)
+
+activation1.forward(dense1.output)
+
+dense2.forward(activation1.output)
+
+activation2.forward(dense2.output)
+
+print(activation2.output[:5])
